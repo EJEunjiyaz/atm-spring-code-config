2bda75e634bf121120c20656006c178f2e0e47d4
Vichyawat Nakarugsa <prince_vichyawat@windowslive.com>
2020-10-08 16:12:41 +0700

    Refactor

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(ATMConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(ATMConfig.class);
         AtmUI atmUI = context.getBean(AtmUI.class);
         atmUI.run();
     }
